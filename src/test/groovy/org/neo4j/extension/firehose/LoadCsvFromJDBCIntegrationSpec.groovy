fe541ff2c083080d662518c6590e423eb0f6dad2
Stefan Armbruster <stefan@armbruster-it.de>
2015-09-05 11:36:57 +0200

    moving setup of h2 db into test rule

--- src/test/groovy/org/neo4j/extension/firehose/LoadCsvFromJDBCIntegrationSpec.groovy
+++ src/test/groovy/org/neo4j/extension/firehose/LoadCsvFromJDBCIntegrationSpec.groovy
@@ -1,9 +1,10 @@
 package org.neo4j.extension.firehose
 
-import groovy.sql.Sql
+import org.junit.ClassRule
 import org.junit.Rule
 import org.neo4j.extension.spock.Neo4jServerResource
 import org.neo4j.graphdb.QueryExecutionException
+import spock.lang.Shared
 import spock.lang.Specification
 
 import static java.net.URLEncoder.encode
@@ -16,20 +17,11 @@ class LoadCsvFromJDBCIntegrationSpec extends Specification {
            thirdPartyJaxRsPackages: [ "org.neo4j.extension.firehose" : "/csv"]
     )
 
+    @ClassRule
+    @Shared
+    SampleDbRule sampleDb = new SampleDbRule()
+
     def "import from h2 database"() {
-
-         def db = Sql.newInstance("jdbc:h2:mem:devDb")
-        db.execute("""
-create table person (
-    id int auto_increment primary key,
-    name varchar(20)
-
-)""")
-        db.execute("insert into person (name) values (?)", ["John"])
-        assert db.updateCount == 1
-
-        db.execute("insert into person (name) values (?)", ["Jim"])
-        assert db.updateCount == 1
 
         when:
         """load csv with headers from '${baseUrl}${url}' as line create (:Person{id:line.ID, name:line.NAME})""".cypher()
@@ -38,13 +30,10 @@ create table person (
         notThrown(QueryExecutionException)
         "match (n:Person) return count(n) as n".cypher()[0].n == count
 
-        cleanup:
-        db.execute("drop table person")
-
         where:
-        url                                                                                 | count
-        "csv/jdbc?url=jdbc:h2:mem:devDb&table=Person"                                           | 2
-        "csv/jdbc?url=jdbc:h2:mem:devDb&sql=${encode("select * from Person limit 1", 'UTF-8')}" | 1
+        url                                                                                    | count
+        "csv/jdbc?url=${sampleDb.jdbc}&table=Person"                                           | 2
+        "csv/jdbc?url=${sampleDb.jdbc}&sql=${encode("select * from Person limit 1", 'UTF-8')}" | 1
 
     }
 
---
+++ src/test/groovy/org/neo4j/extension/firehose/SampleDbRule.groovy
@@ -0,0 +1,36 @@
+package org.neo4j.extension.firehose
+
+import groovy.sql.Sql
+import org.junit.rules.ExternalResource
+
+/**
+ * @author Stefan Armbruster
+ */
+class SampleDbRule extends ExternalResource {
+
+    Sql db
+    String jdbc = "jdbc:h2:mem:devDb"
+
+    @Override
+    protected void before() throws Throwable {
+        db = Sql.newInstance(jdbc)
+        db.execute("""
+create table person (
+   id int auto_increment primary key,
+   name varchar(20)
+
+)""")
+        db.execute("insert into person (name) values (?)", ["John"])
+        assert db.updateCount == 1
+
+        db.execute("insert into person (name) values (?)", ["Jim"])
+        assert db.updateCount == 1
+
+    }
+
+    @Override
+    protected void after() {
+        db?.execute("drop table person")
+        db?.close()
+    }
+}
